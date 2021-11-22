9790ae038f2c93b156f6c345376b51cd5d313b7d
Mateus Silveira Ribeiro <76142179+mateussr7@users.noreply.github.com>
2021-11-22 16:55:20 -0300

    Update costCalculator.py

--- Caminho minimo/utils/costCalculator.py
+++ Caminho minimo/utils/costCalculator.py
@@ -5,9 +5,5 @@ def calc_cost(dist: List[int], target):
     return dist[target]
 
 
-def calc_cost_from_matrix(dist: List[List[int]], path: List[int]):
-    cost = 0
-    for i in range(len(path) - 1):
-        cost += dist[path[i]][path[i + 1]]
-
-    return cost
+def calc_cost_from_matrix(dist: List[List[int]], source: int, target: int):
+    return dist[source][target]
